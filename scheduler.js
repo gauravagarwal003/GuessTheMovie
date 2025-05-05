3c644df9b86889fe98bf15e7832cac808591675a
gauravagarwal003 <gagarwal003@gmail.com>
2025-05-05 10:31:03 -0700

    Update movie data for 5/5/2025

--- scheduler.js
+++ scheduler.js
@@ -192,7 +192,7 @@ async function downloadMoviesData() {
   }
 }
 
-cron.schedule('0 21 * * *', async () => {
+cron.schedule('31 10 * * *', async () => {
   console.log("Cron job triggered at " + new Date().toLocaleString());
   try {
     await downloadMoviesData();
