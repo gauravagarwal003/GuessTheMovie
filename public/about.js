6730c554ba6ca6aa46a2d7d10e16b1414c2d0629
gauravagarwal003 <gagarwal003@gmail.com>
2025-08-25 12:59:55 -0700

    Added contact us button to footer

--- public/about.js
+++ public/about.js
@@ -1,17 +1,37 @@
 function displayContactForm() {
   const modalContentDiv = document.getElementById('modalContent');
   
-  modalContentDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1QkFlmLqevhyPgnqCRA-nj3yLsb0lQxgA_BGFtxbZySnNVA/viewform?embedded=true" width="640" height="915" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`;
+  modalContentDiv.innerHTML = `
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1QkFlmLqevhyPgnqCRA-nj3yLsb0lQxgA_BGFtxbZySnNVA/viewform?embedded=true" 
+      width="640" height="915" frameborder="0" marginheight="0" marginwidth="0">
+      Loading…
+    </iframe>
+  `;
+
   const modal = document.getElementById('Modal');
   modal.style.display = "block";
 
-    // If x button is clicked or outside of modal is clicked, close modal
+  // Remove the #openModal from the URL after opening
+  if (window.location.hash === "#openModal") {
+    history.replaceState(null, "", window.location.pathname + window.location.search);
+  }
+
+  // Close modal when "x" is clicked
   document.getElementById('closeModal').onclick = function () {
     modal.style.display = "none";
   };
+
+  // Close modal when clicking outside
   window.onclick = function (event) {
     if (event.target === modal) {
       modal.style.display = "none";
     }
   };
 }
+
+// Auto-open if URL contains #openModal
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.location.hash === "#openModal") {
+    displayContactForm();
+  }
+});
